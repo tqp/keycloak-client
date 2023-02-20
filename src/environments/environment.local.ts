d5d74b2ba9ab5870dff2035e4e18ebe149ce503a
Tim Powers <tpowers@ridgelineintl.com>
2023-02-19 22:22:25 -0500

    Endpoint diagnostics are working.

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  env: 'local',
+  apiUrl: 'http://localhost:5000',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  env: 'unknown',
+  apiUrl: 'http://localhost:5000',
+};
